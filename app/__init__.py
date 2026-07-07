f7b99aa7e9cb57330839e81db48d0ad4c3abf9b6
Samuele Pellizzieri <samuelepellizzieri@MAC-70733F.station>
2026-07-07 16:46:58 +0200

    Aggiunge migrazione database per confezioni e descrizione prodotto

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,19 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 db = SQLAlchemy()
+migrate = Migrate()
 
 def create_app():
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_object("config.Config")
     db.init_app(app)
+    migrate.init_app(app, db)
+
     from .routes import main
     app.register_blueprint(main)
+
     with app.app_context():
         from . import models
-        db.create_all()
     return app
